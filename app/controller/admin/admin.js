09e0cf668200473c99f100781a461ff6a55a7683
aogg <524383495@qq.com>
2018-09-06 19:56:21 +0800

    改为客户端渲染，服务端渲染会报错

--- app/controller/admin/admin.js
+++ app/controller/admin/admin.js
@@ -6,8 +6,8 @@ class AdminController extends Controller {
     // app/web/framework/element_admin/views/layout/mixin/ResizeHandler.js
     // 就算修好document也会报一个其他的错误：
     // [admin/admin.js] server render bundle error, try client render, the server render error TypeError: Cannot read property 'name' of undefined
-    await this.ctx.render('admin/admin.js');
-    // await this.ctx.renderClient('admin/admin.js');
+    // await this.ctx.render('admin/admin.js');
+    await this.ctx.renderClient('admin/admin.js');
   }
 
 }
