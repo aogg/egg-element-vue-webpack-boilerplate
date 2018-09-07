41add67d105214b52a57a3328d28e03311310f90
aogg <524383495@qq.com>
2018-09-07 17:01:46 +0800

    处理tinymce第三方插件

--- config/config.default.js
+++ config/config.default.js
@@ -23,9 +23,12 @@ module.exports = app => {
     dir: path.join(app.baseDir, 'logs')
   };
 
-  exports.static = {
+  exports.static = { // 可以公开访问的目录
     prefix: '/public/',
-    dir: path.join(app.baseDir, 'public')
+    dir: [ // 按顺序优先级加载
+      path.join(app.baseDir, 'app/web/asset/public'),
+      path.join(app.baseDir, 'public'),
+    ]
   };
 
   exports.keys = '123456';
@@ -35,11 +38,17 @@ module.exports = app => {
     'access'
   ];
 
-  exports.locals = {
+  exports.locals = { // app/web/view/layout.html文件（vue模板文件）的参数
     layoutOptions: {
       title: 'egg-element-vue-webpack-boilerplate',
       keywords: 'egg, vue, webpack, element, boilerplate, server side render',
       description: 'egg-element-vue-webpack-boilerplate description',
+      css: [
+        { href: 'https://cdn.staticfile.org/font-awesome/4.7.0/css/font-awesome.min.css' },
+      ],
+      js: [
+        { src: '/public/tinymce4.7.5/tinymce.min.js' },
+      ],
     },
   };
 
